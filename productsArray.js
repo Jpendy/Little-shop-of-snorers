ec15d201340de4441a0d54d1fbb9c285f7f64c87
Jpendy <Jpendy256@gmail.com>
2020-04-13 16:23:10 -0700

    created array of objects of products

---
+++ productsArray.js
@@ -0,0 +1,102 @@
+export const products = [
+    {
+        id: 0,
+        name: 'R2D2 Bag',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/bag.jpg?raw=true'  
+    },
+    {
+        id: 1,
+        name: 'Banana Slicer',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/banana.jpg?raw=true'  
+    },
+    {
+        id: 2,
+        name: 'Bathroom Ipad Holder',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/bathroom.jpg?raw=true'  
+    },
+    {
+        id: 3,
+        name: 'Best Boots Ever',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/boots.jpg?raw=true'  
+    },
+    {
+        id: 4,
+        name: 'Breakfast Maker',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/breakfast.jpg?raw=true'  
+    },
+    {
+        id: 5,
+        name: 'Meatball Gum',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/bubblegum.jpg?raw=true'  
+    },
+    {
+        id: 6,
+        name: 'Stupid Chair',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/chair.jpg?raw=true'  
+    },
+    {
+        id: 7,
+        name: 'Cthulu',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/cthulhu.jpg?raw=true'  
+    },
+    {
+        id: 8,
+        name: 'Dog Duck Face',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/dog-duck.jpg?raw=true'  
+    },
+    {
+        id: 9,
+        name: 'Dragon Meat',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/dragon.jpg?raw=true'  
+    },
+    {
+        id: 10,
+        name: 'Silverware Pen',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/pen.jpg?raw=true'  
+    },
+    {
+        id: 11,
+        name: 'Pet Sweeper',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/pet-sweep.jpg?raw=true'  
+    },
+    {
+        id: 12,
+        name: 'Taun Taun sleeping bag',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/tauntaun.jpg?raw=true'  
+    },
+    {
+        id: 13,
+        name: 'Unicorn Meat',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/unicorn.jpg?raw=true'  
+    },
+    {
+        id: 14,
+        name: 'Tentacle USB thumb drive',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/usb.gif?raw=true'  
+    },
+    {
+        id: 15,
+        name: 'Weird Scissors',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/scissors.jpg?raw=true'  
+    },
+    {
+        id: 16,
+        name: 'Shark Sleeping Bag',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/shark.jpg?raw=true'  
+    },
+    {
+        id: 17,
+        name: 'Stupid Water Can',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/water-can.jpg?raw=true'  
+    },
+    {
+        id: 18,
+        name: 'Amazing Wine Glass',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/wine-glass.jpg?raw=true'  
+    },
+    {
+        id: 19,
+        name: 'Baby Sweeper',
+        image: 'https://github.com/alchemycodelab/fsjs-foundations-march-2020/blob/master/curriculum/class-11/lab/assets/sweep.png?raw=true' 
+    }
+];
